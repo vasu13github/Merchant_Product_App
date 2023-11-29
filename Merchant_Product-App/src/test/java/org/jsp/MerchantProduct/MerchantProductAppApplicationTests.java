f6dfffb88799048c974ebcdf3519c0ad1a431d4b
vasu <151111750+vasu13github@users.noreply.github.com>
2023-11-29 20:19:28 +0530

    Add files via upload

---
+++ Merchant_Product-App/src/test/java/org/jsp/MerchantProduct/MerchantProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.MerchantProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantProductAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
